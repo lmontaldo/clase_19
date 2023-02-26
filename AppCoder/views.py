74bfe2311b1c0d4573a5ba04d5f89b4266782230
Laura Montaldo <31391486+lmontaldo@users.noreply.github.com>
2023-02-26 17:21:53 -0300

    Add files via upload

---
+++ AppCoder/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from AppCoder.models import Curso
+from django.http import HttpResponse
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def cursos(request):
+    return render(request, 'cursos.html')
+
+def profesores(request):
+    return render(request, 'profesores.html')
+
+def estudiantes(request):
+    return render(request, 'estudiantes.html')
+
+def entregables(request):
+    return render(request, 'entregables.html')
